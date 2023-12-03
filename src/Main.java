0d5a448fc7d1d21d99d3c95f7d3c9b48cbd678b2
PacaChristianPhilipS27 <cybercion.xtian@gmail.com>
2023-12-03 08:08:21 +0800

    added Abstract package

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+         Dog mydog = new Dog("Oscar");
+         Cat mycat = new Cat("Charlie");
+ 
+        
+        mydog.Color();
+        mycat.Color();
+    }
+}
